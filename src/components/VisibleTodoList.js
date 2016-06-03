9f8b527772188b198972e490540d642eaa4f2128
Cameron Strandberg <cameron@timeline.com>
2016-06-03 12:21:40 -0700

    added selector function to reducer files for adding state to todos comp

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,28 +1,12 @@
 import { connect } from 'react-redux';
 import { toggleTodo } from '../actions';
+import { withRouter } from 'react-router';
+import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter(t => t.completed);
-    case 'active':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
-
-const mapStateToProps = (state, ownProps) => {
-  return {
-    todos: getVisibleTodos(
-      state.todos,
-      ownProps.filter
-    )
-  };
-};
+const mapStateToProps = (state, params) => ({
+  todos: getVisibleTodos(state, params.filter || all)
+});
 
 const VisibleTodoList = connect(
   mapStateToProps,
