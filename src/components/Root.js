2adc768ce643c07873390190250cec59ad769620
Cameron Strandberg <cameron@timeline.com>
2016-06-02 23:59:22 -0700

    Tell my mother I am going home

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
 import App from './App';
 
 const Root = ({store}) => (
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>
 )
 
