97724c1b26fa940cfae0311745f38151f3ed5b12
Cameron Strandberg <cameron@timeline.com>
2016-06-03 22:52:19 -0700

    Updating visible todolist to use component lifecycle hooks against fake api

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,16 +1,33 @@
+
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { toggleTodo } from '../actions';
 import { withRouter } from 'react-router';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
+import {fetchTodos} from '../api';
 
-const mapStateToProps = (state, params) => ({
-  todos: getVisibleTodos(state, params.filter || all)
-});
 
-const VisibleTodoList = connect(
+class VisibleTodoList extends Component {
+  componentDidMount(){
+    fetchTodos(this.props.filter)
+  }
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
+
+const mapStateToProps = (state, params) => {
+  const filter = params.filter || 'all';
+  return {
+      todos: getVisibleTodos(state, filter),
+      filter
+  }
+};
+
+VisibleTodoList = connect(
   mapStateToProps,
   {onTodoClick: toggleTodo}
-)(TodoList);
+)(VisibleTodoList);
 
 export default VisibleTodoList;
