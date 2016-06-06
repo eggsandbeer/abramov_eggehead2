d55bb14650e84839c5adbf8a41680d811e712eed
Cameron Strandberg <cameron@timeline.com>
2016-06-05 22:00:34 -0700

    Recfactoring todos reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,43 @@
 import { combineReducers } from 'redux';
-import todos, * as fromTodos from './todos';
+// import todo from './todo';
+import byId, * as fromId from './byId';
+import createList, * as fromList from './createList';
 
-const todoApp = combineReducers({
-  todos,
-});
+const listByFilter = combineReducers({
+  all: createList('all'),
+  active: createList('active'),
+  completed: createList('completed')
+})
 
-export default todoApp;
+const todos = combineReducers({
+  byId,
+  listByFilter
+})
 
-export const getVisibleTodos = (state, filter) =>
-  fromTodos.getVisibleTodos(state.todos, filter);
+export default todos;
+
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id]);
+
+export const getVisibleTodos = (state, filter) => {
+  const ids = fromList.getIds(state.listByFilter[filter]);
+  return ids.map(id => fromId.getTodo(state.byId, id));
+
+
+
+
+
+
+  // const allTodos = getAllTodos(state);
+  //
+  // switch (filter) {
+  //   case 'all':
+  //     return allTodos;
+  //   case 'completed':
+  //     return allTodos.filter(t => t.completed);
+  //   case 'active':
+  //     return allTodos.filter(t => !t.completed);
+  //   default:
+  //     throw new Error(`Unknown filter: ${filter}.`);
+  // }
+};
